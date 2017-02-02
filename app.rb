5795a2c577e4f59d4b96dd562e686ecad620453f
Renan Lukas <renan.lukas@youse.com.br>
2017-02-02 15:00:32 -0200

    Format

--- app.rb
+++ app.rb
@@ -8,24 +8,24 @@ current_dir = Dir.pwd
 Dir["#{current_dir}/model/*.rb"].each { |file| require file }
 
 use Rack::PostBodyContentTypeParser
-	get '/pizzas' do
-	  @pizzas = Pizza.all.to_json
-	end
+get '/pizzas' do
+  @pizzas = Pizza.all.to_json
+end
 
-	get '/pizzas/:id' do
-	  @pizza = Pizza.find(params[:id])
-	end
+get '/pizzas/:id' do
+  @pizza = Pizza.find(params[:id])
+end
 
-	post '/pizzas' do
-		binding.pry
-	  @pizza = Pizza.create(params[:pizza])
-	end
+post '/pizzas' do
+  binding.pry
+  @pizza = Pizza.create(params[:pizza])
+end
 
-	put '/pizzas/:id' do
-	  @pizza = Pizza.find(params[:id])
-	  @pizza.save!
-	end
+put '/pizzas/:id' do
+  @pizza = Pizza.find(params[:id])
+  @pizza.save!
+end
 
-	delete '/articles/:id' do
-	  Pizza.destroy(params[:id])
-	end
+delete '/articles/:id' do
+  Pizza.destroy(params[:id])
+end
